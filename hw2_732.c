f3764359a7a0e3561aecfb54848d59ea36940333
wcorn <dsk08208@gmail.com>
2022-03-03 16:03:16 +0900

    C_review

---
+++ hw2_732.c
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//void change(float num,int* quater, int* dimes, int* nickels, int* pennis) {
+//	num *= 100;
+//	*quater = num / 25;
+//	num -= *quater*25;
+//	*dimes = num / 10;
+//	num -= *dimes * 10;
+//	*nickels = num / 5;
+//	num -= *nickels * 5;
+//	*pennis = num / 1;
+//	num -= *pennis * 1;
+//}
+//void main()
+//{
+//	int quater;
+//	int dimes;
+//	int nickels;
+//	int pennis;
+//	float num;
+//	printf("Enter the amount of moeny in your poket\t");
+//	scanf_s("%f", &num);
+//	change(num,&quater, &dimes, &nickels, &pennis);
+//	printf("TOTAL VALUE ENTERED:%6.2f\n%10d quarters\n%10d dimes\n%10d nickels\n%10d peenies\n", num, quater, dimes, nickels, pennis);
+//}
